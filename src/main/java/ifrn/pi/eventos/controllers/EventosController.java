55e4d68b21d5cdb9414a957e70637e60ebed2ead
João Pedro fonseca de Medeiros <20211204010070@ifrn.local>
2025-10-07 17:18:00 -0300

    configurando dependências do banco de dados

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -3,12 +3,24 @@ package ifrn.pi.eventos.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ifrn.pi.eventos.models.Evento;
+
 @Controller
 public class EventosController {
 
 	@RequestMapping("/eventos/form")
 	public String form() {
 		return "formEvento";
+	}
+	
+	@RequestMapping("/eventos/salvar")
+	public String salvar(Evento evento) {
+		System.out.println(evento.getNome());
+		System.out.println(evento.getLocal());
+		System.out.println(evento.getData());
+		System.out.println(evento.getHorario());
+		System.out.println("Deu certo!!");
+		return "blz";
 	}
 	
 }
--- src/main/java/ifrn/pi/eventos/controllers/indexController.java
+++ src/main/java/ifrn/pi/eventos/controllers/indexController.java
@@ -3,13 +3,16 @@ package ifrn.pi.eventos.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ifrn.pi.eventos.models.Evento;
+
 @Controller
 public class indexController {
 
 	@RequestMapping("/")
 	public String index() {
-		System.out.println("Chamou o método index");
+		System.out.println();
 		return "home";
+		
 	}
 	
 }
