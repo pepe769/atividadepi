83247604a83f62753abf6e5399ca7cc8dafc2aed
João Pedro fonseca de Medeiros <20211204010070@ifrn.local>
2025-10-09 10:19:10 -0300

    Enviando dados do formulario para a aplicação

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -1,6 +1,7 @@
 package ifrn.pi.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ifrn.pi.eventos.models.Evento;
@@ -12,15 +13,11 @@ public class EventosController {
 	public String form() {
 		return "formEvento";
 	}
-	
-	@RequestMapping("/eventos/salvar")
-	public String salvar(Evento evento) {
-		System.out.println(evento.getNome());
-		System.out.println(evento.getLocal());
-		System.out.println(evento.getData());
-		System.out.println(evento.getHorario());
-		System.out.println("Deu certo!!");
-		return "blz";
+
+	@PostMapping("/eventos")
+	public String adicionar(Evento evento) {
+		System.out.println(evento);
+		return "evento-adicionar";
 	}
-	
+
 }
