9b44a76ff109d14efd881a892ea136d6d259b027
João Pedro fonseca de Medeiros <20211204010070@ifrn.local>
2025-10-10 11:40:24 -0300

    Enviando ultimas modificações

--- src/main/java/ifrn/pi/eventos/controllers/indexController.java
+++ src/main/java/ifrn/pi/eventos/controllers/indexController.java
@@ -3,14 +3,12 @@ package ifrn.pi.eventos.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import ifrn.pi.eventos.models.Evento;
-
 @Controller
 public class indexController {
 
 	@RequestMapping("/")
 	public String index() {
-		System.out.println();
+		System.out.println("chamou o metodo index");
 		return "home";
 		
 	}
